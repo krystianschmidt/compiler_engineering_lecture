3ae9adebe371c2f4f425ed0aab035a0bcb771f5d
Krystian Schmidt <krystian.schmidt@siemens.com>
2022-04-27 12:50:17 +0200

    Work done

--- Scanner/src/com/thecout/lox/TokenType.java
+++ Scanner/src/com/thecout/lox/TokenType.java
@@ -3,21 +3,24 @@ package com.thecout.lox;
 
 public enum TokenType {
     // Single-character tokens.
+    // ( ) { } , . - + ; / *
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
     // One or two character tokens.
+    // ! != = == < <= > >=
     BANG, BANG_EQUAL,
     EQUAL, EQUAL_EQUAL,
     GREATER, GREATER_EQUAL,
     LESS, LESS_EQUAL,
 
-    // Literals.
-    IDENTIFIER, STRING, NUMBER,
-
     // Keywords.
     AND, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
     PRINT, RETURN, TRUE, VAR, WHILE,
 
+    // Literals.
+    IDENTIFIER, STRING, NUMBER,
+
+    // End of file //
     EOF, COMMENT
 }
